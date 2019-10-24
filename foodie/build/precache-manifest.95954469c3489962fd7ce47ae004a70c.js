249d7b1c0f0594f9919ffcaee57b6ff170ac9399
David Isakson <ikeman32@yahoo.com>
2019-10-24 15:36:16 -0700

    build

---
+++ foodie/build/precache-manifest.95954469c3489962fd7ce47ae004a70c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7503ddccc4dfb8a0ef6c0adb434b6f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85def15a519fd1dd0ac0",
+    "url": "/static/css/main.4482b2d3.chunk.css"
+  },
+  {
+    "revision": "d8b09776fcb7e833a091",
+    "url": "/static/js/2.53cab463.chunk.js"
+  },
+  {
+    "revision": "85def15a519fd1dd0ac0",
+    "url": "/static/js/main.6fa8329b.chunk.js"
+  },
+  {
+    "revision": "1ea14cebd0c11563f30c",
+    "url": "/static/js/runtime-main.62b236a7.js"
+  }
+]);
